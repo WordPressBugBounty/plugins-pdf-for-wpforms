d24dfdd9e6924b7cbcf259e8bec74d26ef746bbd
Sean Murphy <sean@iamseanmurphy.com>
2025-08-09 09:18:25 +0000

    6.2.0

---
+++ pdf-for-wpforms/backend/setup.php
@@ -0,0 +1,69 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+class Yeepdf_Setup_Template {
+    function __construct() { 
+        add_action( 'admin_footer', array( $this, 'add_dialog_page' ) );
+    }
+    function add_dialog_page(){
+        global $post;
+        $screen = get_current_screen();
+        if ( $screen && $screen->post_type === 'yeepdf' && in_array( $screen->base, array('post', 'post-new') ) ) {
+            $value = apply_filters("yeepdf_setup_id","",$post->ID);
+            $type = apply_filters("yeepdf_setup_type","");
+            ?>
+            <input type="hidden" id="yeepdf-setup-template_id" value="<?php echo esc_attr($value) ?>">
+            <input type="hidden" id="yeepdf-setup-type" value="<?php echo esc_attr($type) ?>">
+            <div id="yeepdf-setup-template" style="display:none">
+                <div class="yeepdf-setup-container">
+                    <div class="yeepdf-setup-title">
+                        <div class="yeepdf-setup-title-text">
+                            <?php esc_attr_e("Name Your Template","yeepdf-setup-title") ?>
+                        </div>
+                        <div class="yeepdf-setup-title-input">
+                            <input id="yeepdf-setup-form-title" type="text" placeholder="Enter your template name here…" name="">
+                        </div>
+                    </div>
+                    <div class="yeepdf-setup-title">
+                        <div class="yeepdf-setup-title-text">
+                            <?php esc_html_e("Select a Form","yeepdf-setup-title") ?>
+                        </div>
+                        <div class="yeepdf-setup-title-input">
+                            <?php
+                                $forms = apply_filters("yeepdf_setup_forms",array(),$post->ID);
+                            ?>
+                            <select id="yeepdf-setup-form-id">
+                                <?php foreach($forms as $id => $name){
+                                    ?>
+                                    <option value="<?php echo esc_attr($id) ?>"><?php echo esc_html($name) ?></option>
+                                    <?php
+                                } ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="yeepdf-setup-desc">
+                        <div class="yeepdf-setup-desc-title">
+                            Select a Template
+                        </div>
+                        <div class="yeepdf-setup-desc-desc">
+                            To speed up the process, you can select from one of our pre-made templates, start with a blank form or create your own.
+                        </div>
+                    </div>
+                </div>
+                <div class="list-view-templates">
+                    <?php 
+                          $args = array(
+                                "json"=>"",
+                                "img"=>YEEPDF_CREATOR_BUILDER_URL."backend/demo/template1/1.png",
+                                "title"=>"Email templates",
+                                "cat" => array(),
+                                "id"=>0,
+                            );
+                          do_action( "builder_yeepdfs" );
+                           ?>
+                </div>
+            </div>
+            <?php
+        }
+    }
+}
+new Yeepdf_Setup_Template;
---
+++ pdf-for-wpforms/backend/templates/breakpoint.php
@@ -0,0 +1,27 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+add_action("yeepdf_builder_block","superaddons_pdf_builder_block_break_point",100);
+function superaddons_pdf_builder_block_break_point(){
+    ?>
+    <li>
+        <div class="momongaDraggable pro_disable" data-type="break" title=" Pro Version">
+            <i class="dashicons dashicons-editor-break"></i>
+            <div class="yeepdf-tool-text"><?php esc_html_e("Page Break","pdf-for-wpforms") ?></div>
+        </div>
+    </li>
+    <?php
+}
+add_filter( 'yeepdf_builder_block_html', "pdf_builder_block_break_point_load" );
+function pdf_builder_block_break_point_load($type){
+    $type["block"]["break"]["builder"] = '
+    <div class="builder-elements">
+        <div class="builder-elements-content" data-type="break" style="padding: 15px 0;">
+            <div class="page_break"></div>
+        </div>
+    </div>';
+    //Show editor
+    $type["block"]["break"]["editor"]["container"]["show"]= [];
+    $type["block"]["break"]["editor"]["container"]["style"]= [];
+    $type["block"]["break"]["editor"]["inner"]["style"]=[];
+    return $type;
+}
